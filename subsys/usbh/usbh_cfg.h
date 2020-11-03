55ba2528db1e4e3edbb6db3e215133dcb0efbd1d
Sascha Ludwig <sascha.ludwig@student.hs-rm.de>
2020-11-03 15:05:38 +0100

    subsys: add USB Host subsystem
    Add USB Host subsystem.
    USB Host subsystem supports only hub and msc
    class at the moment.
    The USB Host stack is based on the uC/USBH stack,
    which was published under Apache license,
    (https://github.com/SiliconLabs/uC-USBH
     d6544687a481edb8e5cc8dca9e77bf771717b708).
    
    Signed-off-by: Sascha Ludwig <sascha.ludwig@student.hs-rm.de>

---
+++ subsys/usbh/usbh_cfg.h
@@ -0,0 +1,197 @@
+/*
+ * Copyright 2004-2020 Silicon Laboratories Inc. www.silabs.com
+ *
+ * SPDX-License-Identifier: APACHE-2.0
+ *
+ */
+
+#ifndef USBH_CFG_MODULE_PRESENT
+#define USBH_CFG_MODULE_PRESENT
+
+/* ------ USB TOPOLOGY CFG. SEE NOTE #1 AND #2. ------- */
+
+/*  Number of host controllers                          */
+/*  The number of host controllers that will be ...     */
+/*  ... added to the stack.                             */
+#define  USBH_CFG_MAX_NBR_HC                               1u
+
+/*  Maximum number of class driver supported            */
+/*  The maximum number of class driver ...              */
+/*  ... (MSC, HID, etc) you will use and add to the ... */
+/*  ... core. The hub class is mandatory and must be ...*/
+/*  ... accounted in the total number.                  */
+#define  USBH_CFG_MAX_NBR_CLASS_DRVS                       4u
+
+/*  Maximum number of devices                           */
+/*  The maximum number of devices that the USB host ... */
+/*  ... stack can accept. This must be < 127.           */
+#define  USBH_CFG_MAX_NBR_DEVS                            10u
+
+/*  Maximum number of configurations per device         */
+/*  The maximum number of USB configurations that ...   */
+/*  ... each device can contain. Most of the time, ...  */
+/*  ... devices will have only one configuration.       */
+#define  USBH_CFG_MAX_NBR_CFGS                             1u
+
+/*  Maximum number of interfaces per USB configuration. */
+/*  The maximum number of interface per USB ...         */
+/*  ... configurations that each device can contain. ...*/
+/*  ... Simple USB devices will generally have only ... */
+/*  ... one interface. If you plan to use composite ... */
+/*  ... devices, you should increase this value.        */
+#define  USBH_CFG_MAX_NBR_IFS                              2u
+
+/*  Max number of endpoints per interface.              */
+/*  The maximum number of endpoints that an interface...*/
+/*  ... can contain. The number of endpoints greatly ...*/
+/*  ... depends on the device's class, but should ...   */
+/*  ... generally be around two or three.               */
+#define  USBH_CFG_MAX_NBR_EPS                              3u
+
+/*  Maximum configuration descriptor length             */
+/*  The maximum length of the buffer that is used to ...*/
+/*  ... hold the USB configuration descriptor.          */
+#define  USBH_CFG_MAX_CFG_DATA_LEN                       256u
+
+/*  Maximum length of string descriptors                */
+/*  The maximum length for string descriptors.          */
+#define  USBH_CFG_MAX_STR_LEN                            256u
+
+/*  Timeout for standard request (ms)                   */
+/*  Timeout in ms for standard requests to complete.    */
+#define  USBH_CFG_STD_REQ_TIMEOUT                       5000u
+
+/*  Number of retries on stand requests fail            */
+/*  Number of times the stack should retry to ...       */
+/*  ... execute a standard request when it failed.      */
+#define  USBH_CFG_STD_REQ_RETRY                            3u
+
+/*  Maximum number of isochronous descriptor            */
+/*  The maximum number of isochronous descriptor ...    */
+/*  ... that will be shared between all isochronous ... */
+/*  ... endpoints.                                      */
+#define  USBH_CFG_MAX_ISOC_DESC                            1u
+
+/*  Maximum number of extra urb                         */
+/*  The maximum number of extra urb used for streaming. */
+#define  USBH_CFG_MAX_EXTRA_URB_PER_DEV                    1u
+
+/*  Maximum number of USB hub                           */
+/*  The maximum number of external and root hub that ...*/
+/*  ... can be connected.                               */
+#define  USBH_CFG_MAX_HUBS                                 5u
+
+/*  Maximum number of port per USB hub                  */
+/*  The maximum number of supported ports per hub. ...  */
+/*  ... See note #3.                                    */
+#define  USBH_CFG_MAX_HUB_PORTS                            7u
+
+/* Maximum number of connection retries                 */
+/* The maximum number of reset and connection ...       */
+/* ... retries before tearing down a device             */
+#define  USBH_CFG_MAX_NUM_DEV_RECONN                       3u
+
+
+/*
+ * COMMUNICATION DEVICE CLASS (CDC) CONFIGURATION
+ */
+
+/*  Maximum number of CDC device                        */
+/*  The maximum number of CDC devices that can be ...   */
+/*  ... connected at the same time.                     */
+#define  USBH_CDC_CFG_MAX_DEV                              1u
+
+/*  Maximum number of CDC ACM device                    */
+/*  The maximum number of CDC ACM devices that can ...  */
+/*  ... be connected at the same time.                  */
+#define  USBH_CDC_ACM_CFG_MAX_DEV                          1u
+
+
+/*
+ * HUMAN INTERFACE DEVICE (HID) CLASS CONFIGURATION
+ */
+
+/*  Maximum number of HID devices                       */
+/*  The maximum number of HID devices that can be ...   */
+/*  ... connected at the same time.                     */
+#define  USBH_HID_CFG_MAX_DEV                              5u
+
+/*  Maximum number of application collections           */
+/*  The maximum number of HID application ...           */
+/*  ... collections supported.                          */
+#define  USBH_HID_CFG_MAX_NBR_APP_COLL                    10u
+
+/*  Maximum number of report IDs                        */
+/*  The maximum number of HID report IDs.               */
+#define  USBH_HID_CFG_MAX_NBR_REPORT_ID                   15u
+
+/*  Maximum number of report formats                    */
+/*  The maximum number of HID report formats.           */
+#define  USBH_HID_CFG_MAX_NBR_REPORT_FMT                  15u
+
+/*  Maximum number of usage in local                    */
+/*  The maximum number of HID usage in local.           */
+#define  USBH_HID_CFG_MAX_NBR_USAGE                       15u
+
+/*  Maximum length of transmission buffer               */
+/*  The maximum length of buffer used for OUT reports.  */
+#define  USBH_HID_CFG_MAX_TX_BUF_SIZE                     64u
+
+/*  Maximum length of reception buffer                  */
+/*  The maximum length of buffer used for IN reports.   */
+#define  USBH_HID_CFG_MAX_RX_BUF_SIZE                    128u
+
+/*  Maximum number of callbacks for device              */
+/*  The maximum length of buffer used for IN reports.   */
+#define  USBH_HID_CFG_MAX_NBR_RXCB                         2u
+
+/*  Maximum length of report descriptor                 */
+/*  The maximum length of buffer used to hold the ...   */
+/*  ... report descriptor.                              */
+#define  USBH_HID_CFG_MAX_REPORT_DESC_LEN                400u
+
+/*  Maximum error count                                 */
+/*  The maximum number of error that can occur.         */
+#define  USBH_HID_CFG_MAX_ERR_CNT                          5u
+
+/*  Maximum global collection for push/pop items        */
+/*  The maximum number of global push/pop items.        */
+#define  USBH_HID_CFG_MAX_GLOBAL                           2u
+
+/*  Maximum collections for open/close collection       */
+/*  The maximum number of collections for open/close ...*/
+/*  ... collection.                                     */
+#define  USBH_HID_CFG_MAX_COLL                            10u
+
+
+/*
+ * MASS STORAGE CLASS (MSC) CONFIGURATION
+ */
+
+/*  Maximum number of MSC devices                       */
+/*  The maximum number of MSC devices that can be ...   */
+/*  ... connected at the same time.                     */
+#define  USBH_MSC_CFG_MAX_DEV                              2u
+
+
+/*
+ * FUTURE TECHNOLOGY DEVICES INTERNATIONAL (FTDI) CONFIGURATION
+ */
+
+/*  Maximum number of FTDI devices                      */
+/*  The maximum number of FTDI devices that can be ...  */
+/*  ... connected at the same time.                     */
+#define  USBH_FTDI_CFG_MAX_DEV                             3u
+
+/*  Custom vendor ID for FTDI device                    */
+/*  Specifies a custom vendor ID for a FTDI device ...  */
+/*  ... to accept.                                      */
+#define  USBH_FTDI_CFG_ID_VENDOR_CUSTOM               0x0000u
+
+/*  Custom product ID for FTDI device                   */
+/*  Specifies a custom product ID for a FTDI device ... */
+/*  ... to accept.                                      */
+#define  USBH_FTDI_CFG_ID_PRODUCT_CUSTOM              0x0000u
+
+#endif
+
